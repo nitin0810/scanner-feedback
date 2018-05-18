2a41cdfe6edd4483d91b993d25050eb2f6945bd9
akash-gupta-146 <ag.akash007@gmail.com>
2018-05-18 12:13:14 +0530

    updated designs

--- src/pages/sort-filter-options/sort-filter.ts
+++ src/pages/sort-filter-options/sort-filter.ts
@@ -10,14 +10,14 @@ import { IonicPage, ActionSheetController } from 'ionic-angular';
         <ion-row>
             <ion-col no-padding>
                 <button full ion-button (click)="onSort()" [style.background-color]="sortSelected ? 'grey': ''" icon-start>
-                    <ion-icon name="arrow-round-down"></ion-icon><ion-icon name="arrow-round-up"></ion-icon>
+                    <ion-icon  name="arrow-round-down"></ion-icon><ion-icon  name="arrow-round-up"></ion-icon>
                 Sort By
                 </button>
             </ion-col>
 
             <ion-col no-padding>
                     <button full ion-button (click)="onFilter()" [style.background-color]="filterSelected ? 'grey': ''" icon-start>
-                        <ion-icon name="funnel"></ion-icon>
+                        <ion-icon  name="funnel"></ion-icon>
                     Filter By
                     </button>
             </ion-col>  
